a82305fa99060f8b95fd019ffaace8f627382ad4
tahavirdiwala <tahavirdiwala97@gmail.com>
2024-12-17 15:27:39 +0530

    inject private methods

--- services/user/user.service.ts
+++ services/user/user.service.ts
@@ -1,21 +1,23 @@
 import { Service } from "typedi";
-import { ResponseHandlers } from "../responser/response-handlers";
 import User from "../../models/user.model";
 import { StatusCodes } from "../../lib/constant";
 import { IUserAuthRequest } from "../../types/express.type";
+import { Responser } from "../responser/response-handlers";
 
 @Service()
-export class UserService extends ResponseHandlers {
+export class UserService {
+  constructor(private responser: Responser) {}
+
   async getAllUsers(req: IUserAuthRequest) {
     try {
       const data = await User.find();
-      return this.sendResponse(
+      return this.responser.sendResponse(
         "Users fetched successfully",
         StatusCodes.OK,
         data
       );
     } catch (error) {
-      return this.catchHandler(error as Error);
+      return this.responser.catchHandler(error as Error);
     }
   }
 }
