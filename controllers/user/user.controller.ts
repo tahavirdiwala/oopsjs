ba5f62f00d39dc27ee24e738308e7783226f5ca4
tahavirdiwala <tahavirdiwala97@gmail.com>
2024-12-17 15:13:52 +0530

    .

--- controllers/user/user.controller.ts
+++ controllers/user/user.controller.ts
@@ -1,15 +1,15 @@
 import { Get, JsonController, Req, Res, UseBefore } from "routing-controllers";
 import { UserService } from "../../services/user/user.service";
-import { ResponseHandlers } from "../../services/responser/response-handlers";
 import { Response } from "express";
 import { authMiddleware } from "../../auth/auth";
 import { IUserAuthRequest } from "../../types/express.type";
+import { ResponseHandler } from "../../services/responser/response-handlers";
 
 @JsonController("/users")
 export class UserController {
   constructor(
     private userService: UserService,
-    private responser: ResponseHandlers
+    private responser: ResponseHandler
   ) {}
 
   @Get("/")
